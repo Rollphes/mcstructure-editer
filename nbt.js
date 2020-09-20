834003bf85d622031a34ce25361217223fa51330
Rollphes <70063670+Rollphes@users.noreply.github.com>
2020-09-21 03:58:03 +0900

    update nbt.js

--- nbt.js
+++ nbt.js
@@ -403,6 +403,7 @@
     var dataView = new DataView(arrayView.buffer);
 
     function read(dataType, size) {
+      console.log(dataType);
       var val = dataView['get' + dataType](self.offset);
       self.offset += size;
       return val;
