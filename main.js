aa59e0b683ae409afba37c1df68a365d779dd72f
Rollphes <70063670+Rollphes@users.noreply.github.com>
2020-09-21 02:44:53 +0900

    update main.js

--- main.js
+++ main.js
@@ -3,5 +3,10 @@ $(function() {
     var file = e.target.files[0];
     var reader = new FileReader();
     console.log('test');
+    reader.onload = function() {
+      var ar = new Uint8Array(reader.result);
+    }
+
+    reader.readAsArrayBuffer(file);
   });
 });
