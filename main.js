d5571d5f97f6300f1ee3c82bf38c5eded72bd751
Rollphes <70063670+Rollphes@users.noreply.github.com>
2020-09-21 16:38:56 +0900

    update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ $(function() {
   $('#file').change(function(e) {
     var file = e.target.files[0];
     var reader = new FileReader();
-    console.log('test');
     reader.onload = function() {
       var data = reader.result
       nbt.parse(data, function(error, data) {
