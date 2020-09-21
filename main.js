dbc0ba3dc88718c5011e9c7f2f3f413da949417e
Rollphes <70063670+Rollphes@users.noreply.github.com>
2020-09-21 20:22:24 +0900

    update main.js

--- main.js
+++ main.js
@@ -9,10 +9,30 @@ $(function() {
           throw error;
         }
 
-        console.log(JSON.stringify(data));
+        copyTextToClipboard(JSON.stringify(data));
       });
     }
 
     reader.readAsArrayBuffer(file);
   });
 });
+function copyTextToClipboard(textVal) {
+  // テキストエリアを用意する
+  var copyFrom = document.createElement("textarea");
+  // テキストエリアへ値をセット
+  copyFrom.textContent = textVal;
+
+  // bodyタグの要素を取得
+  var bodyElm = document.getElementsByTagName("body")[0];
+  // 子要素にテキストエリアを配置
+  bodyElm.appendChild(copyFrom);
+
+  // テキストエリアの値を選択
+  copyFrom.select();
+  // コピーコマンド発行
+  var retVal = document.execCommand('copy');
+  // 追加テキストエリアを削除
+  bodyElm.removeChild(copyFrom);
+  // 処理結果を返却
+  return retVal;
+}
